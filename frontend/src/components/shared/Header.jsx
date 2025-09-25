018156f20d97643ee036c764a30e907acfba07e7
Devanshu-23 <devanshugaur234@gmail.com>
2025-09-26 00:08:35 +0530

    Initial commit

---
+++ frontend/src/components/shared/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="flex-btw ">
+            <div>
+                Header
+            </div>
+        </header>
+    );
+};
+
+export default Header;
