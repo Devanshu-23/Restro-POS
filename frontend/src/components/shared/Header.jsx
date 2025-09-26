a61262f9a07534b9a592a0add78dbe00c5a53964
Devanshu-23 <devanshugaur234@gmail.com>
2025-09-26 09:32:25 +0530

    basic design

--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
+import { FaBell, FaSearch, FaUserCircle } from 'react-icons/fa';
+
 const Header = () => {
     return (
-        <header className="flex-btw ">
-            <div>
-                Header
+        <header className="flex-btw py-4 px-8 bg-[#1a1a1a]">
+            {/* LOGO */}
+            <div className='flex-center gap-2'>
+                <img  className='h-8 w-8' alt='Restro Logo' />
+                <h1 className='title'>Restro</h1>
             </div>
+            {/* SEARCH */}
+            <div className='flex items-center gap-4 bg-[#1f1f1f] rounded-[15px] px-5 py-2 w-[500px]' >
+                <FaSearch className="text-[#f5f5f5] text-2xl" />
+                <input 
+                    type="text" 
+                    placeholder='Search'
+                    className='bg-[#1f1f1f] text-[#f5f5f5] outline-none w-full'
+                />
+
+            </div>
+            {/* LOGGED USER DETAILS */}
+            <div className='flex-center gap-4'>
+                <div className='bg-[#1f1f1f] rounded-[15px] p-3 cursor-pointer' >
+                    <FaBell className="text-[#f5f5f5] text-2xl" />
+                </div>
+                <div className='flex-center gap-3 cursor-pointer'>
+                    <FaUserCircle className="text-[#f5f5f5] text-4xl" />
+                    <div className='flex flex-col items-start'>
+                        <h1 className='text-md text-[#f5f5f5] font-semibold'>Alexis</h1>
+                        <p className='text-xs text-[#ababab] font-medium'>Admin</p>
+                    </div>
+                </div>
+            </div>
+            {/* */}
+            {/* CART */}
+
         </header>
     );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import BottomNav from "../components/shared/BottomNav";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to Restro POS</h1>
-        </div>
+        <section className="flex gap-3 bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden">
+            {/* Left Div */}
+            <div className="flex-[3] bg-[#1a1a1a]"></div>
+
+            <div className="flex-[2]"></div>
+            <BottomNav />
+        </section>
     );
 };
 
